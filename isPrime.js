e6f1c5c8ba8138d9d6e962d3b98d0c60072f4b05
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-09 20:02:14 -0700

    possibly done

--- isPrime.js
+++ isPrime.js
@@ -3,6 +3,17 @@
 
 var isPrime = function (num) {
 
+	for (var i = 2; i < num / 2; i++) {
+		// console.log("i: " + i);
+		// console.log("num % i: " + num % i);
+
+		if (num % i === 0) {
+			// num can be divided by other than 1 and itself
+			return false;
+		} 
+	}
+	// if got through all possible divisors
+	return true;
 
 }
 
--- primes.js
+++ primes.js
@@ -3,14 +3,39 @@
 //  an array of all prime numbers up to a certain amount.
 
 var primes = function(max) {
+// make array
+	var arr = [];
 
-	isPrime(num);
+	for (var i = 0; i < max; i++) {
+
+		if (isPrime(i)) {
+			// add to array
+			arr.push(i); 
+		}
+	}
+	return arr;
+}
+
+var isPrime = function (num) {
+
+	for (var i = 2; i < num / 2; i++) {
+		// console.log("i: " + i);
+		// console.log("num % i: " + num % i);
+
+		if (num % i === 0) {
+			// num can be divided by other than 1 and itself
+			return false;
+		} 
+	}
+	// if got through all possible divisors
+	return true;
 
 }
 
+
 var primesArr = primes(56);
-console.log("All primes up to 56" + primesArr);
+console.log("All primes up to 56: " + primesArr);
 
 
 var primesArr = primes(24);
-console.log("All primes up to 24" + primesArr);
+console.log("All primes up to 24: " + primesArr);
