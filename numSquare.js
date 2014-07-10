75b4be44e5fd5a5c57da7c3cecedf213fc482f68
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-09 19:47:02 -0700

    done with numSquares

--- numSquare.js
+++ numSquare.js
@@ -2,13 +2,18 @@
 // Create a function called numSquare that will return an array of all 
 // perfect square numbers up to, but not exceeding a max number.
 
+
 var numSquare = function(max) {
+	var squaresArr = [];
 
-
+	for (i = 1; (i * i) <= max; i++){
+		squaresArr[i] = i * i;
+	}
+	return squaresArr;
 }
 
 var resultsArr = numSquare(14);
 console.log("Squares up to 14 " + resultsArr);
 
-var resultsArr = numSquare(55);
+resultsArr = numSquare(55);
 console.log("Squares up to 55 " + resultsArr);
